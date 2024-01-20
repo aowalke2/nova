4a8e284a6136123c9df0ed89887aecc4a68a6523
Aza Walker <azawalker.jr@gmail.com>
2024-01-20 18:40:03 -0500

    status bar

--- src/editor.rs
+++ src/editor.rs
@@ -1,11 +1,12 @@
-use std::env;
-
-use crate::document;
 use crate::Document;
 use crate::Row;
 use crate::Terminal;
+use std::env;
+use termion::color;
 use termion::event::Key;
 
+const STATUS_FG_COLOR: color::Rgb = color::Rgb(63, 63, 63);
+const STATUS_BG_COLOR: color::Rgb = color::Rgb(239, 239, 239);
 const VERSION: &str = env!("CARGO_PKG_VERSION");
 
 #[derive(Default)]
@@ -28,11 +29,9 @@ impl Editor {
             if let Err(error) = self.refresh_screen() {
                 die(&error);
             }
-
             if self.should_quit {
                 break;
             }
-
             if let Err(error) = self.process_keypress() {
                 die(&error);
             }
@@ -42,8 +41,8 @@ impl Editor {
     pub fn default() -> Self {
         let args: Vec<String> = env::args().collect();
         let document = if args.len() > 1 {
-            let filename = &args[1];
-            Document::open(&filename).unwrap_or_default()
+            let file_name = &args[1];
+            Document::open(&file_name).unwrap_or_default()
         } else {
             Document::default()
         };
@@ -65,6 +64,8 @@ impl Editor {
             println!("Goodbye.\r");
         } else {
             self.draw_rows();
+            self.draw_status_bar();
+            self.draw_message_bar();
             Terminal::cursor_position(&Position {
                 x: self.cursor_position.x.saturating_sub(self.offset.x),
                 y: self.cursor_position.y.saturating_sub(self.offset.y),
@@ -199,7 +200,7 @@ impl Editor {
 
     fn draw_rows(&self) {
         let height = self.terminal.size().height;
-        for terminal_row in 0..height - 1 {
+        for terminal_row in 0..height {
             Terminal::clear_current_line();
             if let Some(row) = self.document.row(terminal_row as usize + self.offset.y) {
                 self.draw_row(row)
@@ -209,6 +210,37 @@ impl Editor {
                 println!("~\r");
             }
         }
+    }
+
+    fn draw_status_bar(&self) {
+        let mut status;
+        let width = self.terminal.size().width as usize;
+        let mut file_name = "[No Name]".to_string();
+        if let Some(name) = &self.document.file_name {
+            file_name = name.clone();
+            file_name.truncate(20)
+        }
+        status = format!("{} - {} lines", file_name, self.document.len());
+        let line_indicator = format!(
+            "{}/{}",
+            self.cursor_position.y.saturating_add(1),
+            self.document.len()
+        );
+        let len = status.len() + line_indicator.len();
+        if width > len {
+            status.push_str(&" ".repeat(width - len));
+        }
+        status = format!("{}{}", status, line_indicator);
+        status.truncate(width);
+        Terminal::set_bg_color(STATUS_BG_COLOR);
+        Terminal::set_fg_color(STATUS_FG_COLOR);
+        print!("{}\r", status);
+        Terminal::reset_fg_color();
+        Terminal::reset_bg_color();
+    }
+
+    fn draw_message_bar(&self) {
+        Terminal::clear_current_line();
     }
 }
 
